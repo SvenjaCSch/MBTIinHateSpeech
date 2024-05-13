4c048459fb2e4fdf070c9bd0ea829ce333ab307f
SvenjaCSch <73709681+SvenjaCSch@users.noreply.github.com>
2024-05-13 11:27:11 +0200

    Update 02_stylo_analysis.R

--- 02_stylo_analysis.R
+++ 02_stylo_analysis.R
@@ -1,17 +1,17 @@
 library(stylo)
 
-all <- "C:/Users/svcsc/Desktop/MasterStudium/Semester2/digital humanities/project/mbti/all/corpus" 
-sample1_3 <- "C:/Users/svcsc/Desktop/MasterStudium/Semester2/digital humanities/project/mbti/sample1/corpus"
-sample2_3 <- "C:/Users/svcsc/Desktop/MasterStudium/Semester2/digital humanities/project/mbti/sample2/corpus"
-sample3_3 <- "C:/Users/svcsc/Desktop/MasterStudium/Semester2/digital humanities/project/mbti/sample3/corpus"
-sample4_3 <- "C:/Users/svcsc/Desktop/MasterStudium/Semester2/digital humanities/project/mbti/sample4/corpus"
-sample5_3 <- "C:/Users/svcsc/Desktop/MasterStudium/Semester2/digital humanities/project/mbti/sample5/corpus"
+all <- "../all/corpus" 
+sample1_3 <- "../sample1/corpus"
+sample2_3 <- "../sample2/corpus"
+sample3_3 <- "../sample3/corpus"
+sample4_3 <- "../sample4/corpus"
+sample5_3 <- "../sample5/corpus"
 
-sample1_10 <- "C:/Users/svcsc/Desktop/MasterStudium/Semester2/digital humanities/project/mbti/sample1_10/corpus"
-sample2_10 <- "C:/Users/svcsc/Desktop/MasterStudium/Semester2/digital humanities/project/mbti/sample2_10/corpus"
-sample3_10 <- "C:/Users/svcsc/Desktop/MasterStudium/Semester2/digital humanities/project/mbti/sample3_10/corpus"
-sample4_10 <- "C:/Users/svcsc/Desktop/MasterStudium/Semester2/digital humanities/project/mbti/sample4_10/corpus"
-sample5_10 <- "C:/Users/svcsc/Desktop/MasterStudium/Semester2/digital humanities/project/mbti/sample5_10/corpus"
+sample1_10 <- "../sample1_10/corpus"
+sample2_10 <- "../sample2_10/corpus"
+sample3_10 <- "../sample3_10/corpus"
+sample4_10 <- "../sample4_10/corpus"
+sample5_10 <- "../sample5_10/corpus"
 
 make_stylo_analysis <- function(path){
   stylo(corpus.dir = path, analysis.type = "CA", mfw.min = 100, mfw.max = 1000, distance.measure = "dist.cosine", save.distance.tables = TRUE, save.analyzed.features = TRUE, save.analyzed.freqs = TRUE, write.png.file = TRUE, gui = FALSE)
@@ -22,26 +22,26 @@ make_stylo_analysis <- function(path){
   stylo(corpus.dir = path, analysis.type = "CA", ngram.size = 2, mfw.min = 100, mfw.max = 100, distance.measure = "dist.cosine",  save.distance.tables = TRUE, save.analyzed.features = TRUE, save.analyzed.freqs = TRUE, write.png.file = TRUE, gui = FALSE)
 }
 
-setwd("C:/Users/svcsc/Desktop/MasterStudium/Semester2/digital humanities/project/mbti/sample1")
+setwd("../sample1")
 make_stylo_analysis(sample1_3)
-setwd("C:/Users/svcsc/Desktop/MasterStudium/Semester2/digital humanities/project/mbti/sample2")
+setwd("../sample2")
 make_stylo_analysis(sample2_3)
-setwd("C:/Users/svcsc/Desktop/MasterStudium/Semester2/digital humanities/project/mbti/sample3")
+setwd("../sample3")
 make_stylo_analysis(sample3_3)
-setwd("C:/Users/svcsc/Desktop/MasterStudium/Semester2/digital humanities/project/mbti/sample4")
+setwd("../sample4")
 make_stylo_analysis(sample4_3)
-setwd("C:/Users/svcsc/Desktop/MasterStudium/Semester2/digital humanities/project/mbti/sample5")
+setwd("../sample5")
 make_stylo_analysis(sample5_3)
 
-setwd("C:/Users/svcsc/Desktop/MasterStudium/Semester2/digital humanities/project/mbti/sample1_10")
+setwd("../sample1_10")
 make_stylo_analysis(sample1_10)
-setwd("C:/Users/svcsc/Desktop/MasterStudium/Semester2/digital humanities/project/mbti/sample2_10")
+setwd("../sample2_10")
 make_stylo_analysis(sample2_10)
-setwd("C:/Users/svcsc/Desktop/MasterStudium/Semester2/digital humanities/project/mbti/sample3_10")
+setwd("../sample3_10")
 make_stylo_analysis(sample3_10)
-setwd("C:/Users/svcsc/Desktop/MasterStudium/Semester2/digital humanities/project/mbti/sample4_10")
+setwd("../sample4_10")
 make_stylo_analysis(sample4_10)
-setwd("C:/Users/svcsc/Desktop/MasterStudium/Semester2/digital humanities/project/mbti/sample5_10")
+setwd("../sample5_10")
 make_stylo_analysis(sample5_10)
-setwd("C:/Users/svcsc/Desktop/MasterStudium/Semester2/digital humanities/project/mbti/all")
+setwd("../all")
 make_stylo_analysis(all)
