76d2fa39e8591eda3ca65e5425a1bfa574839f6f
SvenjaCSch <73709681+SvenjaCSch@users.noreply.github.com>
2024-05-13 11:24:48 +0200

    Uploading Files
    1. Dataset to Stylo() analysis to get a first overview of the data. Here, the data has to be cut into badges
    2. stylo analysis via R
    3. Regression with original dataset
    4. Logisitic Regression with additional dataset and prediction of MBTI types

---
+++ 02_stylo_analysis.R
@@ -0,0 +1,47 @@
+library(stylo)
+
+all <- "C:/Users/svcsc/Desktop/MasterStudium/Semester2/digital humanities/project/mbti/all/corpus" 
+sample1_3 <- "C:/Users/svcsc/Desktop/MasterStudium/Semester2/digital humanities/project/mbti/sample1/corpus"
+sample2_3 <- "C:/Users/svcsc/Desktop/MasterStudium/Semester2/digital humanities/project/mbti/sample2/corpus"
+sample3_3 <- "C:/Users/svcsc/Desktop/MasterStudium/Semester2/digital humanities/project/mbti/sample3/corpus"
+sample4_3 <- "C:/Users/svcsc/Desktop/MasterStudium/Semester2/digital humanities/project/mbti/sample4/corpus"
+sample5_3 <- "C:/Users/svcsc/Desktop/MasterStudium/Semester2/digital humanities/project/mbti/sample5/corpus"
+
+sample1_10 <- "C:/Users/svcsc/Desktop/MasterStudium/Semester2/digital humanities/project/mbti/sample1_10/corpus"
+sample2_10 <- "C:/Users/svcsc/Desktop/MasterStudium/Semester2/digital humanities/project/mbti/sample2_10/corpus"
+sample3_10 <- "C:/Users/svcsc/Desktop/MasterStudium/Semester2/digital humanities/project/mbti/sample3_10/corpus"
+sample4_10 <- "C:/Users/svcsc/Desktop/MasterStudium/Semester2/digital humanities/project/mbti/sample4_10/corpus"
+sample5_10 <- "C:/Users/svcsc/Desktop/MasterStudium/Semester2/digital humanities/project/mbti/sample5_10/corpus"
+
+make_stylo_analysis <- function(path){
+  stylo(corpus.dir = path, analysis.type = "CA", mfw.min = 100, mfw.max = 1000, distance.measure = "dist.cosine", save.distance.tables = TRUE, save.analyzed.features = TRUE, save.analyzed.freqs = TRUE, write.png.file = TRUE, gui = FALSE)
+  stylo(corpus.dir = path, analysis.type = "CA", mfw.min = 500, mfw.max = 500, distance.measure = "dist.cosine", save.distance.tables = TRUE, save.analyzed.features = TRUE, save.analyzed.freqs = TRUE, write.png.file = TRUE, gui = FALSE)
+  stylo(corpus.dir = path, analysis.type = "CA", mfw.min = 1000, mfw.max = 100, distance.measure = "dist.cosine",  save.distance.tables = TRUE, save.analyzed.features = TRUE, save.analyzed.freqs = TRUE, write.png.file = TRUE, gui = FALSE)
+  stylo(corpus.dir = path, analysis.type = "CA", ngram.size = 2, mfw.min = 100, mfw.max = 1000, distance.measure = "dist.cosine", save.distance.tables = TRUE, save.analyzed.features = TRUE, save.analyzed.freqs = TRUE, write.png.file = TRUE, gui = FALSE)
+  stylo(corpus.dir = path, analysis.type = "CA", ngram.size = 2, mfw.min = 100, mfw.max = 500, distance.measure = "dist.cosine", save.distance.tables = TRUE, save.analyzed.features = TRUE, save.analyzed.freqs = TRUE, write.png.file = TRUE, gui = FALSE)
+  stylo(corpus.dir = path, analysis.type = "CA", ngram.size = 2, mfw.min = 100, mfw.max = 100, distance.measure = "dist.cosine",  save.distance.tables = TRUE, save.analyzed.features = TRUE, save.analyzed.freqs = TRUE, write.png.file = TRUE, gui = FALSE)
+}
+
+setwd("C:/Users/svcsc/Desktop/MasterStudium/Semester2/digital humanities/project/mbti/sample1")
+make_stylo_analysis(sample1_3)
+setwd("C:/Users/svcsc/Desktop/MasterStudium/Semester2/digital humanities/project/mbti/sample2")
+make_stylo_analysis(sample2_3)
+setwd("C:/Users/svcsc/Desktop/MasterStudium/Semester2/digital humanities/project/mbti/sample3")
+make_stylo_analysis(sample3_3)
+setwd("C:/Users/svcsc/Desktop/MasterStudium/Semester2/digital humanities/project/mbti/sample4")
+make_stylo_analysis(sample4_3)
+setwd("C:/Users/svcsc/Desktop/MasterStudium/Semester2/digital humanities/project/mbti/sample5")
+make_stylo_analysis(sample5_3)
+
+setwd("C:/Users/svcsc/Desktop/MasterStudium/Semester2/digital humanities/project/mbti/sample1_10")
+make_stylo_analysis(sample1_10)
+setwd("C:/Users/svcsc/Desktop/MasterStudium/Semester2/digital humanities/project/mbti/sample2_10")
+make_stylo_analysis(sample2_10)
+setwd("C:/Users/svcsc/Desktop/MasterStudium/Semester2/digital humanities/project/mbti/sample3_10")
+make_stylo_analysis(sample3_10)
+setwd("C:/Users/svcsc/Desktop/MasterStudium/Semester2/digital humanities/project/mbti/sample4_10")
+make_stylo_analysis(sample4_10)
+setwd("C:/Users/svcsc/Desktop/MasterStudium/Semester2/digital humanities/project/mbti/sample5_10")
+make_stylo_analysis(sample5_10)
+setwd("C:/Users/svcsc/Desktop/MasterStudium/Semester2/digital humanities/project/mbti/all")
+make_stylo_analysis(all)
